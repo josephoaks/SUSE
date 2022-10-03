8b28f7379f4f7aa97ffeb22513c3ff78019384d8
Puppet Pirate <josephoaks@users.noreply.github.com>
2022-10-03 12:09:15 -0600

    Update agent_setup.sh

--- Rancher_Scripts/agent_setup.sh
+++ Rancher_Scripts/agent_setup.sh
@@ -9,9 +9,9 @@ if [ -f $file ]; then
 fi
 
 # Install fresh
-TOKEN=`/usr/bin/cat node-token`
+TOKEN='/usr/bin/cat node-token'
 SERVER='https://pi.pirate.com'
-HOSTNAME=`/usr/bin/hostname -f`
+HOSTNAME='/usr/bin/hostname -f'
 K3S='v1.23.8+k3s1'
 
 curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" K3S_URL="$SERVER:6443" K3S_NODE_NAME="$HOSTNAME" INSTALL_K3S_VERSION="$K3S" sh -
