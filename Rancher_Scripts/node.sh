2a6b5c480c976f7a9c8ae4510fda81442c3f1213
Puppet Pirate <josephoaks@users.noreply.github.com>
2022-10-03 17:29:14 -0600

    Update node.sh

--- Rancher_Scripts/node.sh
+++ Rancher_Scripts/node.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+###############################
+# Control-plane Setup Section #
+###############################
+
 # Clean-up to re-install
-file='/usr/local/bin/k3s-uninstall.sh'
-if [ -f $file ]; then
+if [ -f '/usr/local/bin/k3s-uninstall.sh' ]; then
   /usr/local/bin/k3s-uninstall.sh
   rm -rf /var/lib/rancher
   rm -rf /etc/rancher
@@ -10,8 +13,8 @@ if [ -f $file ]; then
 fi
 
 # Variable initialization
-SERVER='pi.pirate.com'
-K3S='v1.23.8+k3s1'
+SERVER=`hostname -f`
+read -ep "Enter K3S version you wish to install (v1.23.8+k3s1): " K3S
 
 # Install fresh
 dir='/root/.kube'
@@ -26,8 +29,41 @@ cp -rip /etc/rancher/k3s/k3s.yaml $dir/config
 
 TOKEN=`/usr/bin/cat /var/lib/rancher/k3s/server/node-token`
 
-scp /var/lib/rancher/k3s/server/node-token pi1:/root/node-token
-scp /var/lib/rancher/k3s/server/node-token pi2:/root/node-token
+#######################
+# Agent Setup Section #
+#######################
+echo ""
+echo "#"
+echo "# Agent Setup Section"
+echo "#"
+echo ""
+echo "Enter the agent nodes names, etc fqdn1 fqdn2 fqdn3"
+read -a details
+len=${#details[@]}
 
-ssh pi1 '/bin/sh /root/agent_setup.sh'
-ssh pi2 '/bin/sh /root/agent_setup.sh'
+for (( i=0; i<$len; i++)); do
+HOSTNAME=${details[$i]}
+###########################
+# Make Agent Setup Script #
+###########################
+cat <<EOF > .agent_setup${details[$i]}.sh
+#!/bin/sh
+
+# Clean-up to re-install
+if [ -f "/usr/local/bin/k3s-agent-uninstall.sh" ]; then
+  /usr/local/bin/k3s-agent-uninstall.sh
+  rm -rf /etc/rancher
+  rm -rf /var/lib/rancher
+fi
+
+# Install fresh
+curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN K3S_NODE_NAME=$HOSTNAME INSTALL_K3S_VERSION=$K3S sh -s agent --server https://$SERVER:6443
+EOF
+#############################
+# End of Agent Setup Script #
+#############################
+
+  scp .agent_setup${details[$i]}.sh root@${details[$i]}:/root/agent_setup.sh;
+  ssh root@${details[$i]} '/bin/sh /root/agent_setup.sh'
+  rm -f .agent_setup${details[$i]}.sh
+done
