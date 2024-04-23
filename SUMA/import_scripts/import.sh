5b27b4a9203149221d05bb7851e86a4855891172
Puppet Pirate <josephoaks@users.noreply.github.com>
2024-04-22 21:28:41 -0600

    Update import.sh

--- SUMA/import_scripts/import.sh
+++ SUMA/import_scripts/import.sh
@@ -1,14 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-yaml_file="/mnt/import/scripts/rgsimportexport.yaml"
-user='rsyncuser'
+# Script Name: import_sync.sh
+#
+# Description: This script synchronizes files from a remote server using rsync,
+#              then processes the directories to perform specific import tasks.
+#              It reads configurations from a YAML file to retrieve host details
+#              and other necessary credentials.
+#
+# Usage: ./import_sync.sh
+#
+# YAML Configuration Requirements:
+#   - host: The hostname or IP address of the remote server.
+#   - pass: The password for the XML-RPC user.
+#
+# Example YAML Content:
+# ---
+# version: 2
+# host: example.com
+# pass: yourpassword
+#
+# Notes: Ensure the YAML file 'rgsimportexport.yaml' is located in the same
+#        directory as this script. Requires rsync, SSH access, and permissions
+#        to read the specified directories.
+
+script_dir="$(dirname "$0")"
+yaml_file="${script_dir}/rgsimportexport.yaml"
 host=$(awk '/^host:/ { print $2 }' "$yaml_file")
 pass=$(awk '/^pass:/ { print $2 }' "$yaml_file")
 uname='rgsimportexport'
 basedir='/mnt/import'
-
-options="--xmlRpcUser=$uname --xmlRpcPassword=$pass --logLevel=error"
-
 log_dir="/mnt/logs"
 log_file="${log_dir}/$(date +"%Y-%m-%d")-import.log"
 
@@ -20,9 +40,15 @@ log() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" >> "$log_file"
 }
 
+# Clear source directory before rsync of new data.
 rm -rf "$basedir"/updates/*
 rm -rf "$basedir"/initial/*
-rsync -avP -e 'ssh -i /home/rsyncuser/.ssh/id_rsa' "${user}@${host}":/ "$basedir"
+
+# SSH Configuration variables.
+ssh_user='rsyncuser'
+ssh_key="id_rsa"
+ssh_options="-i /home/${ssh_user}/.ssh/${ssh_key}"
+rsync -avP -e "ssh ${ssh_options}" "${ssh_user}@${host}":/ "$basedir"
 
 process_directory() {
   if [ -z "$(find "$1" -mindepth 1 -type d -print -quit)" ]; then
@@ -30,7 +56,8 @@ process_directory() {
     log "No imports at this time for $dir_name."
     return
   fi
-
+  
+  local options="--xmlRpcUser=$uname --xmlRpcPassword=$pass --logLevel=error"
   for dir in "$1"/*; do
     if [ -d "$dir" ]; then
       inter-server-sync import --importDir="$dir" $options >> "$log_file" 2>&1
